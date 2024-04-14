e6fd1444b062850bcb6d0f995ee4a9b07fda1d4f
ph0109 <pf6350@gmail.com>
2024-04-14 22:38:05 +0900

    addbirthdayhj

--- signUp.js
+++ signUp.js
@@ -1,23 +1,3 @@
-// 휴대폰 번호 입력 부분
-function changePhone1(){
-    const phone1 = document.getElementById("phone1").value
-    if(phone1.length === 3){
-        document.getElementById("phone2").focus();
-    }
-}
-function changePhone2(){
-    const phone2 = document.getElementById("phone2").value
-    if(phone2.length === 4){
-        document.getElementById("phone3").focus();
-    }
-}
-function changePhone3(){
-    const phone3 = document.getElementById("phone3").value
-    if(phone3.length === 4){
-    }
-}
-
-//let processID = -1;
 
 // 가입부분 체크
 
@@ -57,31 +37,6 @@ function signUpCheck(){
     document.getElementById("nameError").innerHTML=""
   }
 
-
-  // 비밀번호 확인
-  if(password !== passwordCheck){
-    document.getElementById("passwordError").innerHTML=""
-    document.getElementById("passwordCheckError").innerHTML="비밀번호가 동일하지 않습니다."
-    check = false
-  }else{
-    document.getElementById("passwordError").innerHTML=""
-    document.getElementById("passwordCheckError").innerHTML=""
-  }
-
-  if(password===""){
-    document.getElementById("passwordError").innerHTML="비밀번호를 입력해주세요."
-    check = false
-  }else{
-    //document.getElementById("passwordError").innerHTML=""
-  }
-  if(passwordCheck===""){
-    document.getElementById("passwordCheckError").innerHTML="비밀번호를 다시 입력해주세요."
-    check = false
-  }else{
-    //document.getElementById("passwordCheckError").innerHTML=""
-  }
-
-
   // 성별체크확인
   if(!gender_man && !gender_woman){
     document.getElementById("genderError").innerHTML="성별을 선택해주세요."
@@ -94,69 +49,12 @@ function signUpCheck(){
     document.getElementById("emailError").innerHTML=""
     document.getElementById("nameError").innerHTML=""
     document.getElementById("passwordError").innerHTML=""
-    document.getElementById("passwordCheckError").innerHTML=""
-    document.getElementById("areaError").innerHTML=""
     
     //비동기 처리이벤트
     setTimeout(function() {
       alert("가입이 완료되었습니다.")
   },0);
   }
-}
-
-function sample4_execDaumPostcode() {
-  new daum.Postcode({
-      oncomplete: function(data) {
-          // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
-
-          // 도로명 주소의 노출 규칙에 따라 주소를 표시한다.
-          // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
-          var roadAddr = data.roadAddress; // 도로명 주소 변수
-          var extraRoadAddr = ''; // 참고 항목 변수
-
-          // 법정동명이 있을 경우 추가한다. (법정리는 제외)
-          // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
-          if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
-              extraRoadAddr += data.bname;
-          }
-          // 건물명이 있고, 공동주택일 경우 추가한다.
-          if(data.buildingName !== '' && data.apartment === 'Y'){
-             extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
-          }
-          // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
-          if(extraRoadAddr !== ''){
-              extraRoadAddr = ' (' + extraRoadAddr + ')';
-          }
-
-          // 우편번호와 주소 정보를 해당 필드에 넣는다.
-          document.getElementById('sample4_postcode').value = data.zonecode;
-          document.getElementById("sample4_roadAddress").value = roadAddr;
-          document.getElementById("sample4_jibunAddress").value = data.jibunAddress;
-          
-          // 참고항목 문자열이 있을 경우 해당 필드에 넣는다.
-          if(roadAddr !== ''){
-              document.getElementById("sample4_extraAddress").value = extraRoadAddr;
-          } else {
-              document.getElementById("sample4_extraAddress").value = '';
-          }
-
-          var guideTextBox = document.getElementById("guide");
-          // 사용자가 '선택 안함'을 클릭한 경우, 예상 주소라는 표시를 해준다.
-          if(data.autoRoadAddress) {
-              var expRoadAddr = data.autoRoadAddress + extraRoadAddr;
-              guideTextBox.innerHTML = '(예상 도로명 주소 : ' + expRoadAddr + ')';
-              guideTextBox.style.display = 'block';
-
-          } else if(data.autoJibunAddress) {
-              var expJibunAddr = data.autoJibunAddress;
-              guideTextBox.innerHTML = '(예상 지번 주소 : ' + expJibunAddr + ')';
-              guideTextBox.style.display = 'block';
-          } else {
-              guideTextBox.innerHTML = '';
-              guideTextBox.style.display = 'none';
-          }
-      }
-  }).open();
 }
 
 function change_btn(e){
@@ -171,3 +69,54 @@ function change_btn(e){
   });
   console.log(e.currentTarget);
 }
+
+const birthYearEl = document.querySelector('#birth-year')
+isYearOptionExisted = false;
+birthYearEl.addEventListener('focus', function() {
+  if(!isYearOptionExisted) {
+    isYearOptionExisted = true
+    for(var i = 1950; i<=2024; i++){
+      const YearOption = document.createElement('option')
+      YearOption.setAttribute('value', i)
+      YearOption.innerText = i
+      this.appendChild(YearOption);
+    }
+  }
+});
+
+const birthMonthEl = document.querySelector('#birth-month')
+isMonthOptionExisted = false;
+birthMonthEl.addEventListener('focus', function() {
+  if(!isMonthOptionExisted) {
+    isMonthOptionExisted = true
+    for(var i = 1; i<=12; i++){
+      const MonthOption = document.createElement('option')
+      MonthOption.setAttribute('value', i)
+      MonthOption.innerText = i
+      this.appendChild(MonthOption);
+    }
+  }
+});
+
+const birthDayEl = document.querySelector('#birth-day')
+isDayOptionExisted = false;
+birthDayEl.addEventListener('focus', function() {
+  if(!isDayOptionExisted) {
+    isDayOptionExisted = true
+    for(var i = 1; i<=31; i++){
+      const DayOption = document.createElement('option')
+      DayOption.setAttribute('value', i)
+      DayOption.innerText = i
+      this.appendChild(DayOption);
+    }
+  }
+});
+
+/** 
+window.onload = function(){
+  var hw = document.getElementById('hw');
+  hw.addEventListener('click', function(){
+    alert('Hello world');
+  })
+}
+*/
